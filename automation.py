f95486cc5b2a51ad5f78bbe9676cbb5e2a46db41
Victor Sullivan <vssullivan@gmail.com>
2020-01-12 23:57:11 -0800

    Added find Phone Number feature.

--- automation.py
+++ automation.py
@@ -2,20 +2,33 @@
 
 # Functions for using Automation
 
-def spam():
-	eggs = 'spam and eggs'
-	print(eggs)
-
-def bacon():
-	eggs = 'bacon and eggs'
-	print(eggs)
-	print("Now we call the spam() in the next line: \n")
-	spam()
-	print("\n\nNow we call the print(spam()) in the next line: \n")
-	print(spam())
 
 # Phone Number Validation
 def isPhoneNumber(text):
-	if len(text) != 12:
-		return False
-	for i in range(0, 3)
+    if len(text) != 12:
+        return False
+    for i in range(0, 3):
+        if not text[i].isdecimal():
+            return False
+    if text[3] != "-":
+        return False
+    for i in range(4, 7):
+        if not text[i].isdecimal():
+            return False
+    if text[7] != "-":
+        return False
+    for i in range(8, 12):
+        if not text[8].isdecimal():
+            return False
+    return True
+
+def findPhoneNumber(message):
+    for num in range(len(message)):
+        phoneChunk = message[num:num+12]
+        if isPhoneNumber(phoneChunk):
+            print("Phone number found: ", + phoneChunk)
+
+
+
+
+
