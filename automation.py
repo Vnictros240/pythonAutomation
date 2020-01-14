2df98e10370d317c26c23c3c8bb0525bed51b308
Victor Sullivan <vssullivan@gmail.com>
2020-01-13 18:46:23 -0800

    Adding a English Dictionary to my Python project.

--- automation.py
+++ automation.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 # Functions for using Automation
-
+# import time
+# import os
+# import pandas
+import json
 
 # Phone Number Validation
 def isPhoneNumber(text):
@@ -29,6 +32,32 @@ def findPhoneNumber(message):
             print("Phone number found: ", + phoneChunk)
 
 
+def fileChecker():
+    while True:
+        if os.path.exists("files/vegatables.txt"):
+            with open("files/vegatables.txt") as file:
+                print(file.read())
+        else:
+            print("File does not exist.")
+        time.sleep(10)
 
+def avgTemps():
+        if os.path.exists("temps_today.csv"):
+            data = pandas.read_csv("temps_today.csv")
+            print(data.mean()["st1"])
+            return data
+        else:
+            print("File does not exist.")
+        time.sleep(10)
 
+# Creating an English Dictionary
+# requires 'import json'
+def translate(word):
+    if word in data[word]:
+        return data[word]
+    else:
+        return "Word doesn't exist."
 
+def englishDictionary():
+    word = input("Enter a word: ")
+    print()
